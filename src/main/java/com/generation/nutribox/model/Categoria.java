57a5f5e6e9fa14be2d71798ba429ee5a1a11e77d
Eliana Morato <liamorato@hotmail.com>
2024-05-24 14:15:25 -0300

    Envio do projeto

---
+++ src/main/java/com/generation/nutribox/model/Categoria.java
@@ -0,0 +1,50 @@
+package com.generation.nutribox.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Nome é obrigatório e não pode estar vazio")
+	@Size(min = 4, max = 50, message = "O campo nome precisa ter entre 4 e 50 caracteres")
+	private String nome;
+	
+	@Size(min = 4, max = 1000, message = "O campo descrição precisa ter entre 4 e 50 caracteres")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
---
+++ src/test/java/com/generation/nutribox/NutriboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.nutribox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
