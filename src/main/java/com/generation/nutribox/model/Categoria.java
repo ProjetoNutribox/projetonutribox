e7746c070fd5e0eed2f0a38546629c1c54dd4ac3
vinicius <vinicius.belucci@outlook.com>
2024-05-29 10:49:00 -0300

    Task 5 - Criação completa do segundo CRUD Envio da criação da Model de Usuário e Model de Produto e Classe Controller e Interface Repository da Model de Produto com o CRUD completo e o método específico de buscar por nome.
    Criação do relacionamento entre as models de Produto e Categoria. #

--- src/main/java/com/generation/nutribox/model/Categoria.java
+++ src/main/java/com/generation/nutribox/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.nutribox.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -16,12 +23,16 @@ public class Categoria {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotBlank(message = "O atributo Nome é obrigatório e não pode estar vazio")
+	@NotBlank(message = "O atributo nome é obrigatório e não pode estar vazio")
 	@Size(min = 4, max = 50, message = "O campo nome precisa ter entre 4 e 50 caracteres")
 	private String nome;
 	
 	@Size(min = 4, max = 1000, message = "O campo descrição precisa ter entre 4 e 50 caracteres")
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("categoria")
+    private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -46,5 +57,13 @@ public class Categoria {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
 }
---
+++ src/main/java/com/generation/nutribox/model/Produto.java
@@ -0,0 +1,106 @@
+package com.generation.nutribox.model;
+
+import org.apache.logging.log4j.util.StringBuilderFormattable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório e não pode estar vazio")
+	@Size(min = 3, max = 255, message = "O campo nome precisa ter entre 3 e 255 caracteres")
+	private String nome;
+	
+	@NotBlank(message = "O atributo tamanho é obrigatório e não pode estar vazio")
+	@Size(min = 3, max = 255, message = "O campo tamanho precisa ter entre 3 e 255 caracteres")
+	private String tamanho;
+	
+	@NotNull(message = "O atributo preco é obrigatório e não pode estar vazio")
+	@Positive
+	private double preco;
+	
+	@NotNull(message = "O atributo quantidade é obrigatório e não pode estar vazio")
+	@PositiveOrZero
+	private Long quantidade;
+	
+	@Size(min = 3, max = 5000, message = "O campo foto precisa ter entre 3 e 5000 caracteres")
+	private String foto;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public Long getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Long quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
